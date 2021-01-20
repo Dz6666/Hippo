6cd0971fd7a25981a8baca50b41bcb197eddadbf
Dz6666 <1284808408@qq.com>
2021-01-20 13:30:44 +0800

    项目初始化结束

---
+++ hippo-web/src/components/ShowCenter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="show_center">
+      <h1>展示中心页面</h1>
+      <a-button type="danger">
+        Danger
+      </a-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShowCenter"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- hippo-web/src/router/index.js
+++ hippo-web/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+// import 起包别名 from 功能包中抛出的功能 
+import Router from 'vue-router'   // ./router是一个包名称。都在node_modules中安装着了，此包有一个功能通过前端访问路径找到对应的组件 export default {被抛出的功能才能被引入}
+
+// 通过路径找到对应的vue组件对象  @表示src/components/HelloWord.vue文件
 import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
-
+// 想要使用功能必须use一下。才可以使用路由的功能
+Vue.use(Router);
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
-  ]
+    },
+
+  ],
 })
+
