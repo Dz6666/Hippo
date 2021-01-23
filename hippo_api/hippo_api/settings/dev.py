21744d8567bf09b88ae4003efe1723bcb4296c7b
unknown <user.name>
2021-01-23 13:30:36 +0800

    django后端项目初始化结束

--- hippo_api/hippo_api/settings/dev.py
+++ hippo_api/hippo_api/settings/dev.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'mbpveamqfyd3&3_#6n6$#()!%r640nv#=bhv(a3mt$mu&_72t8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'web.toptops.top',
+    'api.toptops.top',
+]
 
 
 import sys
@@ -40,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #放在中间件的最上面，就是给响应头加上了一个响应头跨域
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +56,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS组的配置信息
+CORS_ORIGIN_WHITELIST = (
+    #'www.hippo.cn:8080', #如果这样写不行的话，就加上协议(http://www.hippo.cn:8080，因为不同的corsheaders版本可能有不同的要求)
+    'http://web.toptops.top:8080',
+)
+CORS_ALLOW_CREDENTIALS = False  # 是否允许ajax跨域请求时携带cookie，False表示不用，我们后面也用不到cookie，所以关掉它就可以了，以防有人通过cookie来搞我们的网站
 
 ROOT_URLCONF = 'hippo_api.urls'
 
@@ -77,9 +89,13 @@ WSGI_APPLICATION = 'hippo_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "218.244.158.229",
+        "PORT": 3306,
+        "USER": "python",
+        "PASSWORD": "python",
+        "NAME": "hippo",
     }
 }
 
@@ -121,3 +137,58 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 日志配置
+LOGGING = {
+    'version': 1,  #使用的python内置的logging模块，那么python可能会对它进行升级，所以需要写一个版本号，目前就是1版本
+    'disable_existing_loggers': False, #是否去掉目前项目中其他地方中以及使用的日志功能，但是将来我们可能会引入第三方的模块，里面可能内置了日志功能，所以尽量不要关闭。
+    'formatters': { #日志记录格式
+        'verbose': { #levelname等级，asctime记录时间，module表示日志发生的文件名称，lineno行号，message错误信息
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': { #过滤器：可以对日志进行输出时的过滤用的
+        'require_debug_true': { #在debug=True下产生的一些日志信息，要不要记录日志，需要的话就在handlers中加上这个过滤器，不需要就不加
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'require_debug_false': { #和上面相反
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': { #日志处理方式，日志实例
+        'console': { #在控制台输出时的实例
+            'level': 'DEBUG', #日志等级；debug是最低等级，那么只要比它高等级的信息都会被记录
+            'filters': ['require_debug_true'], #在debug=True下才会打印在控制台
+            'class': 'logging.StreamHandler', #使用的python的logging模块中的StreamHandler来进行输出
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            # 日志位置,日志文件名,日志保存目录必须手动创建
+            'filename': os.path.join(os.path.dirname(BASE_DIR), "logs/hippo.log"), #注意，你的文件应该有读写权限。
+            # 日志文件的最大值,这里我们设置300M
+            'maxBytes': 300 * 1024 * 1024,
+            # 日志文件的数量,设置最大日志数量为10
+            'backupCount': 10,
+            # 日志格式:详细格式
+            'formatter': 'verbose',
+            'encoding': 'utf-8',  # 设置默认编码，否则打印出来汉字乱码
+        },
+    },
+    # 日志对象
+    'loggers': {
+        'django': {  #和django结合起来使用，将django中之前的日志输出内容的时候，按照我们的日志配置进行输出，
+            'handlers': ['console', 'file'], #将来项目上线，把console去掉
+            'propagate': True, #冒泡：是否将日志信息记录冒泡给其他的日志处理系统，工作中都是True，不然django这个日志系统捕获到日志信息之后，其他模块中可能也有日志记录功能的模块，就获取不到这个日志信息了
+        },
+    }
+}
+
+REST_FRAMEWORK = {
+    # 异常处理
+    'EXCEPTION_HANDLER': 'hippo_api.utils.exceptions.custom_exception_handler',
+}
